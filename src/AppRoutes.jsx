12f28694085c2f4c8d7f2bc5c6a15cfd0b1ec760
Luis flores <luis.r.f.e@hotmail.com>
2024-03-19 09:49:03 -0600

    redux en marcha

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,30 @@
+import { LoginPage } from "./auth/pages/LoginPage"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Userrouters } from "./routes/Userrouters"
+import { useContext } from "react"
+import { AuthContext } from "./auth/context/AuthContext"
+
+export const AppRoutes = () => {
+
+    const { login } = useContext(AuthContext);
+    return (
+
+        <Routes>
+            {login.isAuth ?
+                (<>
+                    <Route path="/*" element={<Userrouters />} />
+                </>)
+                :
+                <>
+                    <Route path="/login" element={<LoginPage
+
+                    />} />
+                    <Route path="/*" element={<Navigate to={'/login'} />} />
+                </>
+
+            }
+
+        </Routes>
+
+    )
+}
